d45453b549d91233841fe1c977af5187235c4844
Ahnafd <ahnafmd550@gmail.com>
2025-02-24 12:55:04 -0600

    commit after making the home section

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('btn-1').addEventListener('click', function (event) {
+    event.preventDefault();
+    const accountNumber =document.getElementById('input-1').value ;
+    const accountPin =document.getElementById('input-2').value ;
+    if (accountNumber.length===11) {
+        if (accountPin.length===4) {
+    window.location.href = "./home.html"
+
+    }
+    else {
+        alert('pin is incorrect');
+        
+    }
+    }
+    else {
+        alert('error');
+        
+    }
+    
+ })
